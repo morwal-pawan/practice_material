d66f4aeac0c87654842ee66c7ac2c5ffc13de89f
Pavan Mahawar <pawanmahavir350@gmail.com>
2018-11-06 14:10:10 +0530

    Add Header and Footer in layouts

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const Footer = props => {
+  return (
+    <Fragment>
+      <p> Footer.js</p>
+    </Fragment>
+  );
+};
+export default Footer;
---
+++ src/components/layouts/Header.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const Header =(props)=>{
+ return(
+     <Fragment>
+      <p> Header.js</p>
+     </Fragment>
+ )
+}
+export default Header;
