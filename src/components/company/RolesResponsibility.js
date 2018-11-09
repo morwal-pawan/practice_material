e4f10e5d8173ca3b01582776fe2259c26df851ef
Pavan Mahawar <pawanmahavir350@gmail.com>
2018-11-09 15:34:45 +0530

    Add RoleResponsibility.js

--- src/components/company/RolesResponsibility.js
+++ src/components/company/RolesResponsibility.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { Fragment } from "react";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core";
-import {roles_responsibilities} from '../../data/test_data'
+import { roles_responsibilities } from "../../data/test_data";
 
 const styles = theme => ({});
 
-const RolesResponsibility = ({ responsibilities=roles_responsibilities }) => {
-  console.log(responsibilities)
+const RolesResponsibility = ({ responsibilities = roles_responsibilities }) => {
   return (
-    <Typography paragraph>
-      Add rice and stir very gently to distribute. Top with artichokes and
-      peppers, and cook without stirring, until most
-    </Typography>
+    <Fragment>
+      {responsibilities.map((value, index) => {
+        return (
+          <Typography variant="caption">
+            {index}. {value}
+          </Typography>
+        );
+      })}
+    </Fragment>
   );
 };
 
