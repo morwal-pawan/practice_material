a2752d77112fce02ca4367718f7c35cf2c49e543
Pavan Mahawar <pawanmahavir350@gmail.com>
2018-11-11 21:31:18 +0530

    modify Header

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,44 +1,22 @@
 import React, { Fragment } from "react";
-import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
-import classnames from "classnames";
-import Collapse from "@material-ui/core/Collapse";
-import IconButton from "@material-ui/core/IconButton";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { company_reviews } from "../../data/test_data";
 import red from "@material-ui/core/colors/red";
-import CheckCircle from "@material-ui/icons/CheckCircle";
 import ThumbDown from "@material-ui/icons/ThumbDown";
 import ThumbUp from "@material-ui/icons/ThumbUp";
 import Button from "@material-ui/core/Button";
 import BookMark from "@material-ui/icons/Bookmark";
+import IconButton from "@material-ui/core/IconButton";
 
 const styles = theme => ({
-  title: {
-    fontSize: 15,
-    fontWeight: 600
-  },
-  text: {
-    fontSize: 12
-  },
   icon: {
     margin: theme.spacing.unit * 2
   },
-
   iconHover: {
     margin: theme.spacing.unit * 2,
     "&:hover": {
       color: red[800]
     }
-  },
-  typography: {
-    display: "flex",
-    fontSize: 10
-  },
-  userIcon: {
-    margin: 9,
-    fontSize: 13
   },
   button: {
     margin: theme.spacing.unit,
@@ -54,29 +32,16 @@ const styles = theme => ({
 const Footer = ({ classes }) => {
   return (
     <Grid container direction="row" justify="center" alignItems="center">
-      <Button variant="fab" color="primary" className={classes.button}>
-        <ThumbDown className={classes.buttonUserIcon} />
-      </Button>
-      <Button variant="fab" color="primary" className={classes.button}>
-        <BookMark className={classes.buttonUserIcon} />
-      </Button>
-      <Button variant="fab" color="primary" className={classes.button}>
-        <ThumbUp className={classes.buttonUserIcon} />
-      </Button>
+      <IconButton color="secondary">
+        <ThumbDown fontSize="medium" />
+      </IconButton>
+      <IconButton variant="fab" color="primary">
+        <BookMark fontSize="medium" />
+      </IconButton>
+      <IconButton variant="fab" color="primary" >
+        <ThumbUp fontSize="medium" />
+      </IconButton>
     </Grid>
-    // <Tabs
-    // indicatorColor="primary"
-    // textColor="primary"
-    // >
-    //   {/* <Tab
-    //     icon={<FontIcon className="material-icons">phone</FontIcon>}
-    //     label="RECENTS"
-    //   /> */}
-    //   <Tab icon={<Phone />} label="RECENTS" />
-    //   <Tab icon={<Alarm />} label="ACTIVITY" />
-    //   <Tab icon={<Favorite />} label="FAVORITES" />
-    //   <Tab icon={<MapsPersonPin />} label="NEARBY" />
-    // </Tabs>
   );
 };
 export default withStyles(styles)(Footer);
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,22 +1,81 @@
-import React, { Fragment } from "react";
-import { AppBar, Toolbar } from "material-ui";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Badge from "@material-ui/core/Badge";
+import red from "@material-ui/core/colors/red";
+import Settings from "@material-ui/icons/Settings";
+import BookMark from "@material-ui/icons/Bookmark";
 import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
 
-const Header = props => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    height: 100
+  },
+  badge: {
+    top: 1,
+    right: -15,
+    // The border color match the background color.
+    border: `2px solid ${
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[900]
+    }`
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 600
+  },
+  text: {
+    fontSize: 12
+  },
+  icon: {
+    margin: theme.spacing.unit * 2
+  },
+
+  iconHover: {
+    margin: theme.spacing.unit * 2,
+    "&:hover": {
+      color: red[800]
+    }
+  },
+  typography: {
+    display: "flex",
+    fontSize: 10
+  }
+});
+
+const Header = ({ classes }) => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography
-          variant="subheading"
-          align="center"
-          gutterBottom={true}
-          color="inherit"
-        >
-          Headline
+    <AppBar position="static" color="default">
+      <Grid
+        className={classes.root}
+        container
+        direction="row"
+        justify="flex-end"
+        alignItems="center"
+      >
+        <Typography variant="h6" color="inherit">
+          04 Jobs for you
         </Typography>
-      </Toolbar>
+        <IconButton color="secondary" className={classes.button}>
+          <Settings fontSize="medium" />
+        </IconButton>
+        <IconButton color="secondary" className={classes.button}>
+          <Badge className={classes.margin} badgeContent={4} color="primary">
+            <BookMark />
+          </Badge>
+        </IconButton>
+      </Grid>
     </AppBar>
   );
 };
-export default Header;
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
